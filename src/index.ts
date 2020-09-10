21ad8d4e057d52f12c3577319719d338c9009d14
Luis Hernandez <luauhern@gmail.com>
2020-09-10 06:18:48 -0400

    Set up mikro-ORM
    Abstract the env into a constant file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello there");
+import { MikroORM } from "@mikro-orm/core";
+import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
+  const post = orm.em.create(Post, { title: "First post" });
+  orm.em.persistAndFlush(post);
+};
+
+main();
